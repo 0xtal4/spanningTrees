b07a774efe9bb9c4bd42f058b1c6c71deea11d6a
tal <ykvnkl2@gmail.com>
2022-05-01 22:28:00 +0300

    add linked list node

--- Node.py
+++ Node.py
@@ -1,4 +1,10 @@
 class Node():
-    def __init__(self, key):
-        self.key = key
-    
+    def __init__(self, _key, _next):
+        self.next = _next
+        self.key = _key
+
+    def get_next(self):
+        return self.next
+
+    def set_next(self, _next):
+        self.next = _next
--- WeightedGraph.py
+++ WeightedGraph.py
@@ -1,1 +1,4 @@
-
+from Node import Node
+class WeightedGraph():
+    def __init__(self):
+        
